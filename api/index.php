91960a6eaa92dce297e0cf6f4f5f45f2a75830a9
MrPink925 <cpinkster3@gmail.com>
2024-06-21 13:27:13 -0400

    quick change

--- api/index.php
+++ api/index.php
@@ -4,13 +4,15 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Index Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
 </head>
 
 <body>
-  <h1>This is the index page. Your name is <?= $name ?></h1>
+    <h1>This is the index page. Your name is <?= $name ?></h1>
+
+    <?php var_dump($_SERVER); ?>
 </body>
 
 </html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php $name = getenv('MYNAME'); ?>
+<h1> API details </h2>
+    your name is <?php echo $name; ?>
+    <h2> Routes & Endpoints </h2>
+    <ul>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+    </ul>
